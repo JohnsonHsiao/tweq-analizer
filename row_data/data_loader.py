145098c1e8405b4676b7fdc7e78421a639cee126
Johnson Hsiao <JohnsonHsiao@users.noreply.github.com>
2022-11-13 23:01:15 +0800

    done

--- row_data/data_loader.py
+++ row_data/data_loader.py
@@ -9,24 +9,14 @@ import os
 import datetime as dt
 import time
 import requests
-# sys.path.insert(1, os.path.dirname(__file__) + '/..')
+
 user_home_path = str(Path.home())
 os.makedirs(f'{user_home_path}/tweq-analizer/data_center/',exist_ok=True)
 data_center = f'{user_home_path}/tweq-analizer/data_center/'
 
-link = 'https://quality.data.gov.tw/dq_download_json.php?nid=11549&md5_url=bb878d47ffbe7b83bfc1b41d0b24946e'
-data_request = requests.get(link)
-row_data = pd.DataFrame(data_request .json())  #num of all stock
-print(row_data)
-
-row_data  = row_data .rename(columns={'證券代號':'STOCK_ID', '證券名稱':'NAME'})
-print(row_data )
-historical_data = pd.DataFrame()
-for i in row_data.index:    
-    stock_id = row_data.loc[i, 'STOCK_ID'] + '.TW'
-    data = yf.Ticker(stock_id)
-    df = data.history(period="max")
-    print(df)
-    df.to_csv(f'{data_center}{stock_id}.csv')
-
-    
+def get_stock_id():
+    url = 'https://quality.data.gov.tw/dq_download_json.php?nid=11549&md5_url=bb878d47ffbe7b83bfc1b41d0b24946e'
+    data_request = requests.get(url)
+    row_data = pd.DataFrame(data_request .json())  #num of all stock
+    row_data  = row_data .rename(columns={'證券代號':'STOCK_ID'})
+    return row_data['STOCK_ID'] # 9958 
---
+++ row_data/fama_french_table.py
@@ -0,0 +1,56 @@
+from yahoo_fin.stock_info import *
+import pandas as pd
+from datetime import date, timedelta
+import statsmodels.api as sm
+import getFamaFrenchFactors as gff
+from data_loader import get_stock_id
+import logging
+logging.basicConfig(filename='./tweq-analizer/data_center/loader_server.log', level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def get_ffdata(symbol): # Mkt-RF,SMB,HML,RF,Month_Rtn by month
+    
+    symbol = f'{symbol}.TW'
+    data = get_data(symbol, start_date = datetime.datetime.now() - datetime.timedelta(900))
+    data = data.reset_index(level=0)
+    data.rename(columns={data.columns[0]: 'Date'},inplace=True)
+    ff3_monthly = gff.famaFrench3Factor(frequency='m')
+    ff3_monthly.rename(columns={"date_ff_factors": 'Date'}, inplace=True)
+    ff3_monthly.set_index('Date', inplace=True)
+    data.index = pd.to_datetime(data.Date)
+    stock_returns = data['adjclose'].resample('M').last().pct_change().dropna()
+    stock_returns.name = "Month_Rtn"
+    ff_data = ff3_monthly.merge(stock_returns,on='Date')  # Mkt-RF,SMB,HML,RF,Month_Rtn by month
+    return ff_data, ff3_monthly, data
+
+def create_table(symbol): 
+
+    ff_data, ff3_monthly, data = get_ffdata(symbol)
+    X = ff_data[['Mkt-RF', 'SMB', 'HML']]
+    y = ff_data['Month_Rtn'] - ff_data['RF']
+    X = sm.add_constant(X)
+    ff_model = sm.OLS(y, X).fit()
+    # print(ff_model.summary())
+    logging.info(ff_model.summary())
+    intercept, b1, b2, b3 = ff_model.params #const  Mkt-RF SMB  HML 
+    rf = ff_data['RF'].mean()
+    market_premium = ff3_monthly['Mkt-RF'].mean()
+    size_premium = ff3_monthly['SMB'].mean()
+    value_premium = ff3_monthly['HML'].mean()
+
+    expected_monthly_return = rf + b1 * market_premium + b2 * size_premium + b3 * value_premium 
+    expected_yearly_return = expected_monthly_return * 12
+    print("Expected yearly return: " + str(expected_yearly_return))
+
+    expected_daily_return = expected_monthly_return / 30
+    print(expected_daily_return)
+
+    data['daily_returns']=(data['close'].pct_change())
+    data['abnormal_returns'] = expected_daily_return - data['daily_returns']
+    print(data)
+
+
+if __name__ == '__main__':
+    for name in get_stock_id():
+        create_table(name)
