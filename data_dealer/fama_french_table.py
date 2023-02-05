c3fc3038f4d945ff738d79096e6c7c943b937ad4
Johnson Hsiao <JohnsonHsiao@users.noreply.github.com>
2023-02-05 20:33:38 +0800

    ab donee
    @fangsquare u should reconfirm something that i iterate the ER, because the last version is not actually we want, i think we can discuss tomorrow through the google meet

--- data_dealer/fama_french_table.py
+++ data_dealer/fama_french_table.py
@@ -7,12 +7,13 @@ import os
 user_home_path = str(Path.home())
 data_center = f'{user_home_path}/tweq-analizer/data_center/'
 stock_file = f'{user_home_path}/tweq-analizer/data_center/stock_data/'
-os.makedirs(f'{user_home_path}/tweq-analizer/data_center/fama_data2', exist_ok=True)
-fama_data = f'{user_home_path}/tweq-analizer/data_center/fama_data2/'
+os.makedirs(f'{user_home_path}/tweq-analizer/data_center/fama_data', exist_ok=True)
+fama_data = f'{user_home_path}/tweq-analizer/data_center/fama_data/'
 
 df1 = pd.read_csv(f'{data_center}ff_table.csv', index_col = 'Date')
 ff_table = df1.dropna()
 ff_table['rf'] = ff_table['rf']*0.01
+ff_table.index = ff_table.index.astype('datetime64')
 stock_list = os.listdir(stock_file)
 
 def final_table(ff_table, stock_list):
@@ -22,11 +23,6 @@ def final_table(ff_table, stock_list):
         stock_data.dropna().reset_index(drop=True)
         stock_data['Daily_Return'] = stock_data['Adj Close'].pct_change().dropna()
         stock_data.index = pd.to_datetime(stock_data.index).tz_localize(None)
-        print(ff_table)
-        print(stock_data)
-        ff_table.index = ff_table.index.astype('datetime64')
-        print((ff_table.index))
-        print((stock_data.index))
         joint = ff_table.join(stock_data)
         joint['mkt-rf'] = joint['market'] - joint['rf']
         print(joint)
@@ -36,16 +32,14 @@ def final_table(ff_table, stock_list):
             coefficient.append(factor(i,joint))
         c_table = pd.DataFrame(coefficient, columns = ['const ','mkt-rf','SMB','HML'])
         c_table.index = c_table.index + 1
-
+        print(c_table)
+        final = joint.drop(index = joint.index[0:130]).reset_index(0)
         expected_daily_return = []
         for i in range(11,848,1):
             a = er(i,joint,c_table)
             expected_daily_return.append(a)
-            df4 = pd.DataFrame (expected_daily_return, columns = ['ER'])
-            df4.index = df4.index + 1
-        final = joint.drop(index = joint.index[0:129])
-        final.index = final.index - 129
-        final['abnormal_returns'] = df4['ER'] - final['Daily_Return']
+            final['ER'] = pd.DataFrame (expected_daily_return)
+        final['abnormal_returns'] = final['ER'] - final['Daily_Return']
         print(final)
         final.to_csv(f'{fama_data}{stock}')
 
@@ -60,21 +54,21 @@ def factor(t,joint):
     return c
 
 def rf(t,joint):
-    return joint['rf'].loc[t]
+    return joint['rf'].iloc[t]
 def market_premium(t,joint):
-    return joint['mkt-rf'].loc[t]
+    return joint['mkt-rf'].iloc[t]
 def size_premium(t,joint):
-    return joint['SMB'].loc[t]
+    return joint['SMB'].iloc[t]
 def value_premium(t,joint):
-    return joint['HML'].loc[t]
+    return joint['HML'].iloc[t]
 def get_const(t,c_table):
-    return c_table['const'].loc[t]
+    return c_table['const'].iloc[t]
 def get_mkt(t,c_table):
-    return c_table['mkt-rf'].loc[t]
+    return c_table['mkt-rf'].iloc[t]
 def get_SMB(t,c_table):
-    return c_table['SMB'].loc[t]
+    return c_table['SMB'].iloc[t]
 def get_HML(t,c_table):
-    return c_table['HML'].loc[t]
+    return c_table['HML'].iloc[t]
 def er(t,joint,c_table):
     return rf(t-10,joint) + get_mkt(t-10,c_table) * market_premium(t-10,joint) + get_SMB(t-10,c_table) * size_premium(t-10,joint) + get_HML(t-10,c_table) * value_premium(t-10,joint)
 
